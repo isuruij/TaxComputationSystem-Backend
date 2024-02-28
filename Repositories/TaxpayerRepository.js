87aba1d06b225818ca9bc663eff596282d0e3b2e
isuru <2020uelog@gmail.com>
2024-02-28 12:41:56 +0530

    Add console logs for debugging in TaxpayerRepository.js

--- Repositories/TaxpayerRepository.js
+++ Repositories/TaxpayerRepository.js
@@ -1,14 +1,17 @@
 const bcrypt = require("bcrypt");
 const { Taxpayer } = require("../models");
 const jwt = require("jsonwebtoken");
+const crypto = require("crypto")
 
 module.exports.addTaxpayer = async (obj) => {
   try {
     const hashedPw = await bcrypt.hash(obj.password.toString(), 8);
     var data = obj;
-    // console.log(data)
+    console.log(data)
     data.password = hashedPw;
+    
     const r = await Taxpayer.create(data);
+    console.log("------------")
     console.log(r);
     return { status: true };
   } catch (error) {
