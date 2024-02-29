46ac01a98dc9fbaa5654ce25fc083611f50df087
isuru <2020uelog@gmail.com>
2024-02-29 22:24:02 +0530

    Add PATCH method to CORS configuration

--- index.js
+++ index.js
@@ -11,12 +11,14 @@ const taxpayerRoutes = require("./Routes/TaxpayerRoute");
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
+
+
  
 //middleware
 app.use(cors(
   {
     origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true
   }
 ));
