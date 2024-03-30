2b0373ea4b2d3dee63341739a900a2146d23e6c5
Thimira Deshaka <thimiradeshakashan0220@gmail.com>
2024-03-30 10:24:04 +0530

    This is before pull from auth and email branch

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-const express = require("express")
+const express = require("express");
 const bodyparser = require("body-parser");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
 require("express-async-errors");
+require("dotenv").config();
 
-const db = require("./models")
+const db = require("./models");
 const taxpayerRoutes = require("./Routes/TaxpayerRoute");
-const app = express()
-app.use(express.json())
-app.use(cookieParser())
- 
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
 //middleware
-app.use(cors(
-  {
-    origin: "http://localhost:5173",
+app.use(
+  cors({
+    origin: process.env.FRONTEND_BASE_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
-  }
-));
+    credentials: true,
+  })
+);
 
 app.use(bodyparser.json());
 app.use("/api/taxpayer", taxpayerRoutes);
@@ -28,8 +29,8 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500).send("Something went wrong!");
 });
 
-db.sequelize.sync({force:true}).then(() => {
-  app.listen(3000, () => {  
+db.sequelize.sync({}).then(() => {
+  app.listen(3000, () => {
     console.log("Server running on port 3000");
-  }); 
-});      
+  });
+});
