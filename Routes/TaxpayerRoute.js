f5f7746611a78a88baaaeb097c42ade3168abbd9
isuruij <2020uelog@gmail.com>
2024-04-17 14:01:01 +0530

    Update role-based authentication in TaxpayerRoute.js

--- Routes/TaxpayerRoute.js
+++ Routes/TaxpayerRoute.js
@@ -18,9 +18,9 @@ router.post('/login',TaxpayerController.loginTaxpayer);
 
 router.patch('/verifyemail',TaxpayerController.verifyEmail);
 
-router.get('/getuserbasicdetails/:id',JwtService.verifyuser,JwtService.roleBasedAuth(["taxpayer"]),TaxpayerController.getBasicDetails);
+router.get('/getuserbasicdetails/:id',JwtService.verifyuser,JwtService.roleBasedAuth(["taxpayer","superAdmin","admin"]),TaxpayerController.getBasicDetails);
 
-router.patch('/updatebasicdetails',JwtService.verifyuser,JwtService.roleBasedAuth(["taxpayer"]),TaxpayerController.updateBasicDetails);
+router.patch('/updatebasicdetails',JwtService.verifyuser,JwtService.roleBasedAuth(["taxpayer","superAdmin","admin"]),TaxpayerController.updateBasicDetails);
 
 router.post('/forgot-password',TaxpayerController.forgotPassword);    
 
@@ -28,11 +28,11 @@ router.get('/reset-password/:id/:token',TaxpayerController.resetPassword);
 
 router.post('/addnew-password/:id/:token',TaxpayerController.addNewPassword);
 
-router.get('/getuserincomedetails/:id',JwtService.verifyuser,JwtService.roleBasedAuth(["taxpayer"]),TaxpayerController.getuserincomedetails);
+router.get('/getuserincomedetails/:id',JwtService.verifyuser,JwtService.roleBasedAuth(["taxpayer","superAdmin","admin"]),TaxpayerController.getuserincomedetails);
 
-router.patch('/updateincomedetails',JwtService.verifyuser,JwtService.roleBasedAuth(["taxpayer"]),TaxpayerController.updateincomedetails);
+router.patch('/updateincomedetails',JwtService.verifyuser,JwtService.roleBasedAuth(["taxpayer","superAdmin","admin"]),TaxpayerController.updateincomedetails);
 
-router.get('/getNotifications/:id',JwtService.verifyuser,JwtService.roleBasedAuth(["taxpayer"]),TaxpayerController.getNotifications);
+router.get('/getNotifications/:id',JwtService.verifyuser,JwtService.roleBasedAuth(["taxpayer","superAdmin","admin"]),TaxpayerController.getNotifications);
 
 router.patch('/updatePassword',TaxpayerController.updatePassword);
 
