1eaeacce649251878fb473cae7def383e82a5a2b
isuruij <2020uelog@gmail.com>
2024-04-17 22:06:49 +0530

    updating the login function

--- Repositories/SuperAdminRepository.js
+++ Repositories/SuperAdminRepository.js
@@ -1,9 +1,5 @@
 const bcrypt = require("bcrypt");
-const {
-    SuperAdmin,
-    SecondAdmin,
-  Notification
-} = require("../models");
+const { SuperAdmin, SecondAdmin, Notification } = require("../models");
 const jwt = require("jsonwebtoken");
 
 module.exports.addSuperAdmin = async (obj) => {
@@ -12,8 +8,8 @@ module.exports.addSuperAdmin = async (obj) => {
       where: { userName: obj.userName },
     });
     const existingUser2 = await SecondAdmin.findOne({
-        where: { userName: obj.userName },
-      });
+      where: { userName: obj.userName },
+    });
 
     if (existingUser1 || existingUser2) {
       return { status: false, message: "already registered user" };
@@ -31,33 +27,88 @@ module.exports.addSuperAdmin = async (obj) => {
 };
 
 module.exports.loginSuperAdmin = async (obj) => {
-    try {
-        const superAdmin = await SuperAdmin.findOne({
-          where: {
-            userName: obj.userName,
-          },
-        });
-    
-        if (!superAdmin) {
-          return { status: false, message: "SuperAdmin not found" };
-        }
-    
-        const isMatch = await bcrypt.compare(
-          obj.password.toString(),
-          superAdmin.password
-        );
-    
-        if (!isMatch) {
-          return { status: false, message: "Invalid credentials" };
-        } else {
-          return {
-            status: true,
-            name: superAdmin.dataValues.name,
-            id: superAdmin.dataValues.id,
-          };
-        }
-      } catch (error) {
-        console.error("Error in login:", error);
-        throw error;
+  try {
+    const superAdmin = await SuperAdmin.findOne({
+      where: {
+        userName: obj.userName,
+      },
+    });
+
+    if (!superAdmin) {
+      return { status: false, message: "SuperAdmin not found" };
+    }
+
+    const isMatch = await bcrypt.compare(
+      obj.password.toString(),
+      superAdmin.password
+    );
+
+    if (!isMatch) {
+      return { status: false, message: "Invalid credentials" };
+    } else {
+      return {
+        status: true,
+        name: superAdmin.dataValues.name,
+        id: superAdmin.dataValues.id,
+      };
+    }
+  } catch (error) {
+    console.error("Error in login:", error);
+    throw error;
+  }
+};
+
+module.exports.loginSuperAdminnew = async (obj) => {
+  try {
+    const superAdmin = await SuperAdmin.findOne({
+      where: {
+        userName: obj.userName,
+      },
+    });
+
+    if (superAdmin) {
+      const isMatch = await bcrypt.compare(
+        obj.password.toString(),
+        superAdmin.password
+      );
+
+      if (!isMatch) {
+        return { status: false, message: "Invalid credentials" };
+      } else {
+        return {
+          status: true,
+          name: superAdmin.dataValues.name,
+          id: superAdmin.dataValues.id,
+        };
       }
-  };
+    }
+
+    const secondAdmin = await SecondAdmin.findOne({
+      where: {
+        userName: obj.userName,
+      },
+    });
+
+    if (secondAdmin) {
+      const isMatch = await bcrypt.compare(
+        obj.password.toString(),
+        secondAdmin.password
+      );
+
+      if (!isMatch) {
+        return { status: false, message: "Invalid credentials" };
+      } else {
+        return {
+          status: true,
+          name: secondAdmin.dataValues.name,
+          id: secondAdmin.dataValues.id,
+        };
+      }
+    }
+
+    return { status: false, message: "SuperAdmin not found" };
+  } catch (error) {
+    console.error("Error in login:", error);
+    throw error;
+  }
+};
