58f46ed754afda333b102b42449e5d4418cc405a
isuruij <2020uelog@gmail.com>
2024-06-19 12:43:25 +0530

    added default values

--- models/Policies.js
+++ models/Policies.js
@@ -24,9 +24,24 @@ module.exports = (sequelize, DataTypes) => {
       const count = await Policies.count();
       if (count === 0) {
           const initialData = [
-              { title: 'Policy A', amount: 1000.0, rate: 1.5 },
-              { title: 'Policy B', amount: 2000.0, rate: 2.0 },
-              { title: 'Policy C', amount: 1500.0, rate: 1.8 },
+              { title: 'personal relief for 9 months', amount:2250000, rate: 0 },
+              { title: 'personal relief for 3 months', amount: 300000, rate: 0 },
+              { title: 'Rent relief', amount:0, rate: 25 },
+              { title: 'Expenditure relief for 9 months', amount:900000, rate: 0 },
+              { title: 'Income tax first 2,250,000 for 9 months', amount:0, rate:6 },
+              { title: 'Income tax next 2,250,000 for 9 months', amount:0, rate:12 },
+              { title: 'Income tax (remain balance)balance for 9 months', amount:0, rate:18 },
+              { title: 'Income tax first 125,000 for 3 months', amount:0, rate:6 },
+              { title: 'Income tax next 125,000 for 3 months', amount:0, rate:12 },
+              { title: 'Income tax next 125,000 for 3 months', amount:0, rate:18 },
+              { title: 'Income tax next 125,000 for 3 months', amount:0, rate:24 },
+              { title: 'Income tax next 125,000 for 3 months', amount:0, rate:30 },
+              { title: 'Income tax next balance for 3 months', amount:0, rate:36 },
+              { title: 'Capital gain tax rate', amount:0, rate:10 },
+              { title: 'Capital gain tax rate', amount:0, rate:10 },
+              { title: 'Tax on terminal benefits 0 – 10,000,000', amount:0, rate:0 },
+              { title: 'Tax on terminal benefits 10,000,000 – 20,000,000', amount:0, rate:6 },
+              { title: 'Tax on terminal benefits exceeding 20,000,000', amount:0, rate:12 }, 
           ];
 
           await Policies.bulkCreate(initialData);
