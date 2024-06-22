1ab3ace798923b89d783169c0bbad789a2c2def2
isuruij <2020uelog@gmail.com>
2024-06-22 13:56:40 +0530

    chore: Update TaxpayerRoute.js to include role-based authentication for updating password

--- Routes/SuperAdminRoute.js
+++ Routes/SuperAdminRoute.js
@@ -13,68 +13,68 @@ router.post('/createfirstadmin',SuperAdminController.addFirstAdmin);
 
 router.post('/login',SuperAdminController.loginSuperAdmin);
 
-router.post('/addnotifications',SuperAdminController.addNotifications);
-router.get("/getusers",SuperAdminController.getTaxpayers);
+router.post('/addnotifications',JwtService.roleBasedAuth(["superAdmin"]),SuperAdminController.addNotifications);
+router.get("/getusers",JwtService.roleBasedAuth(["superAdmin"]),SuperAdminController.getTaxpayers);
 
-router.get("/getBusinessIncome/",SuperAdminController.getBusinessIncome);
+router.get("/getBusinessIncome/",JwtService.roleBasedAuth(["superAdmin"]),SuperAdminController.getBusinessIncome);
 
-router.delete("/deletetaxpayers/:id", SuperAdminController.deleteTaxpayer);
-router.put("/updateUserApprovalStatus", SuperAdminController.toggleApproval);
+router.delete("/deletetaxpayers/:id",JwtService.roleBasedAuth(["superAdmin"]), SuperAdminController.deleteTaxpayer);
+router.put("/updateUserApprovalStatus",JwtService.roleBasedAuth(["superAdmin"]), SuperAdminController.toggleApproval);
 
 //get income details
-router.get('/getbusinessincome/:taxpayerId', SuperAdminController.getBusinessIncomeByTaxpayerId);
-router.get('/getemployeeincome/:taxpayerId', SuperAdminController.getEmployeeIncomeByTaxpayerId);
-router.get('/getinvestmentincome/:taxpayerId', SuperAdminController.getInvestIncomeByTaxpayerId);
-router.get('/getotherincome/:taxpayerId', SuperAdminController.getOtherIncomeByTaxpayerId);
-router.get('/getcapitalvaluegain/:taxpayerId', SuperAdminController.getCapitalValueGain);
-router.get('/getapit/:taxpayerId', SuperAdminController.getApit);
-router.get('/getreliefforexpenditure/:taxpayerId', SuperAdminController.getReliefForExpenditure);
-router.get('/getreliefforrentincome/:taxpayerId', SuperAdminController.getReliefForRentIncome);
-router.get('/getselfassessmentpayment/:taxpayerId', SuperAdminController.getSelfAssessmentPayment);
-router.get('/getTerminalBenefits/:taxpayerId', SuperAdminController.getTerminalBenefits);
-router.get('/getwhtoninvestmentincome/:taxpayerId', SuperAdminController.getWhtOnInvestmentIncome);
-router.get('/getwhtonservicefeereceived/:taxpayerId', SuperAdminController.getWhtOnServiceFeeReceived);
-router.get('/getwhtwhichisnotdeducted/:taxpayerId', SuperAdminController.getWhtWhichIsNotDeducted);
-router.get('/getqualifyingpayments/:taxpayerId', SuperAdminController.getQualifyingPayments);
+router.get('/getbusinessincome/:taxpayerId',JwtService.roleBasedAuth(["superAdmin"]), SuperAdminController.getBusinessIncomeByTaxpayerId);
+router.get('/getemployeeincome/:taxpayerId',JwtService.roleBasedAuth(["superAdmin"]), SuperAdminController.getEmployeeIncomeByTaxpayerId);
+router.get('/getinvestmentincome/:taxpayerId',JwtService.roleBasedAuth(["superAdmin"]), SuperAdminController.getInvestIncomeByTaxpayerId);
+router.get('/getotherincome/:taxpayerId',JwtService.roleBasedAuth(["superAdmin"]), SuperAdminController.getOtherIncomeByTaxpayerId);
+router.get('/getcapitalvaluegain/:taxpayerId',JwtService.roleBasedAuth(["superAdmin"]), SuperAdminController.getCapitalValueGain);
+router.get('/getapit/:taxpayerId',JwtService.roleBasedAuth(["superAdmin"]), SuperAdminController.getApit);
+router.get('/getreliefforexpenditure/:taxpayerId',JwtService.roleBasedAuth(["superAdmin"]), SuperAdminController.getReliefForExpenditure);
+router.get('/getreliefforrentincome/:taxpayerId',JwtService.roleBasedAuth(["superAdmin"]), SuperAdminController.getReliefForRentIncome);
+router.get('/getselfassessmentpayment/:taxpayerId',JwtService.roleBasedAuth(["superAdmin"]), SuperAdminController.getSelfAssessmentPayment);
+router.get('/getTerminalBenefits/:taxpayerId',JwtService.roleBasedAuth(["superAdmin"]), SuperAdminController.getTerminalBenefits);
+router.get('/getwhtoninvestmentincome/:taxpayerId',JwtService.roleBasedAuth(["superAdmin"]),JwtService.roleBasedAuth(["superAdmin"]), SuperAdminController.getWhtOnInvestmentIncome);
+router.get('/getwhtonservicefeereceived/:taxpayerId', JwtService.roleBasedAuth(["superAdmin"]),SuperAdminController.getWhtOnServiceFeeReceived);
+router.get('/getwhtwhichisnotdeducted/:taxpayerId',JwtService.roleBasedAuth(["superAdmin"]), SuperAdminController.getWhtWhichIsNotDeducted);
+router.get('/getqualifyingpayments/:taxpayerId',JwtService.roleBasedAuth(["superAdmin"]), SuperAdminController.getQualifyingPayments);
  
-router.put("/verifyBusinessIncome", SuperAdminController.verifyBusinessIncome);
-router.put("/verifyEmploymentIncome", SuperAdminController.verifyEmploymentIncome);
-router.put("/verifyInvestmentIncome", SuperAdminController.verifyInvestmentIncome);
-router.put("/verifyOtherIncome", SuperAdminController.verifyOtherIncome);
+router.put("/verifyBusinessIncome",JwtService.roleBasedAuth(["superAdmin"]), SuperAdminController.verifyBusinessIncome);
+router.put("/verifyEmploymentIncome",JwtService.roleBasedAuth(["superAdmin"]), SuperAdminController.verifyEmploymentIncome);
+router.put("/verifyInvestmentIncome",JwtService.roleBasedAuth(["superAdmin"]), SuperAdminController.verifyInvestmentIncome);
+router.put("/verifyOtherIncome",JwtService.roleBasedAuth(["superAdmin"]), SuperAdminController.verifyOtherIncome);
 
-router.put("/verifyCapitalValueGain", SuperAdminController.verifyCapitalValueGain);
-router.put("/verifyReliefForExpenditure", SuperAdminController.verifyReliefForExpenditure);
-router.put("/verifyReliefForRentIncome", SuperAdminController.verifyReliefForRentIncome);
-router.put("/verifyQualifyingPayments", SuperAdminController.verifyQualifyingPayments);
-router.put("/verifyTerminalBenefits", SuperAdminController.verifyTerminalBenefits);
-
-
-router.put("/verifyApit", SuperAdminController.verifyApit);
-router.put("/verifyWhtOnServiceFeeReceived", SuperAdminController.verifyWhtOnServiceFeeReceived);
-router.put("/verifyWhtWhichIsNotDeducted", SuperAdminController.verifyWhtWhichIsNotDeducted);
-router.put("/verifyWhtOnInvestmentIncome", SuperAdminController.verifyWhtOnInvestmentIncome);
-router.put("/verifySelfAssessmentPayment", SuperAdminController.verifySelfAssessmentPayment);
+router.put("/verifyCapitalValueGain",JwtService.roleBasedAuth(["superAdmin"]), SuperAdminController.verifyCapitalValueGain);
+router.put("/verifyReliefForExpenditure",JwtService.roleBasedAuth(["superAdmin"]), SuperAdminController.verifyReliefForExpenditure);
+router.put("/verifyReliefForRentIncome",JwtService.roleBasedAuth(["superAdmin"]), SuperAdminController.verifyReliefForRentIncome);
+router.put("/verifyQualifyingPayments",JwtService.roleBasedAuth(["superAdmin"]), SuperAdminController.verifyQualifyingPayments);
+router.put("/verifyTerminalBenefits",JwtService.roleBasedAuth(["superAdmin"]), SuperAdminController.verifyTerminalBenefits);
 
 
-router.post("/requestDocument",SuperAdminController.requestDocument);
-router.post("/requestAgainDocument",SuperAdminController.requestAgainDocument);
+router.put("/verifyApit",JwtService.roleBasedAuth(["superAdmin"]), SuperAdminController.verifyApit);
+router.put("/verifyWhtOnServiceFeeReceived",JwtService.roleBasedAuth(["superAdmin"]), SuperAdminController.verifyWhtOnServiceFeeReceived);
+router.put("/verifyWhtWhichIsNotDeducted", JwtService.roleBasedAuth(["superAdmin"]),SuperAdminController.verifyWhtWhichIsNotDeducted);
+router.put("/verifyWhtOnInvestmentIncome", JwtService.roleBasedAuth(["superAdmin"]),SuperAdminController.verifyWhtOnInvestmentIncome);
+router.put("/verifySelfAssessmentPayment",JwtService.roleBasedAuth(["superAdmin"]), SuperAdminController.verifySelfAssessmentPayment);
 
 
-router.patch('/updatePolicy',SuperAdminController.updatePolicy);
+router.post("/requestDocument",JwtService.roleBasedAuth(["superAdmin"]),SuperAdminController.requestDocument);
+router.post("/requestAgainDocument",JwtService.roleBasedAuth(["superAdmin"]),SuperAdminController.requestAgainDocument);
 
-router.delete('/deletePolicy',SuperAdminController.deletePolicy);
 
-router.get('/policy',SuperAdminController.policy);
+router.patch('/updatePolicy',JwtService.roleBasedAuth(["superAdmin"]),SuperAdminController.updatePolicy);
+
+router.delete('/deletePolicy',JwtService.roleBasedAuth(["superAdmin"]),SuperAdminController.deletePolicy);
+
+router.get('/policy',JwtService.roleBasedAuth(["superAdmin"]),SuperAdminController.policy);
 
 // optional policies
 
-router.get('/optionalpolicy',SuperAdminController.optionalpolicy);
+router.get('/optionalpolicy',JwtService.roleBasedAuth(["superAdmin"]),SuperAdminController.optionalpolicy);
 
-router.patch('/updateoptionalpolicy',SuperAdminController.updateoptionalpolicy);
+router.patch('/updateoptionalpolicy',JwtService.roleBasedAuth(["superAdmin"]),SuperAdminController.updateoptionalpolicy);
 
-router.post('/createPolicy',SuperAdminController.createPolicy);
+router.post('/createPolicy',JwtService.roleBasedAuth(["superAdmin"]),SuperAdminController.createPolicy);
 
-router.get("/authtsuperAdmin", JwtService.authtsuperAdmin, SuperAdminController.authtsuperAdmin);
+router.get("/authtsuperAdmin",JwtService.roleBasedAuth(["superAdmin"]), JwtService.authtsuperAdmin, SuperAdminController.authtsuperAdmin);
 
 
  
